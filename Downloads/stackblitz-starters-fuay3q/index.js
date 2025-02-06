3925a23227f1ca17eb086e1d32dc5c6fceaa14f9
megha-r20 <megha202005@gmail.com>
2025-02-06 10:43:53 +0530

    done

--- Downloads/stackblitz-starters-fuay3q/index.js
+++ Downloads/stackblitz-starters-fuay3q/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { resolve } = require('path');
-const config = require('./config')
 require('dotenv').config();
+const config = require('./config')
 
 const app = express();
 const port = 3010;
 
+console.log(config)
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
