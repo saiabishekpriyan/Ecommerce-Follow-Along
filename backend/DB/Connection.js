df89a99d3a794a9a83e515b5dda9e02e7ed0d62e
saiabishekpriyan <saiabishek.priyan.s81@gmail.com>
2025-02-21 11:24:04 +0530

    Done

--- backend/DB/Connection.js
+++ backend/DB/Connection.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose")
 require("dotenv").config()
-const connection =mongoose.connect(process.env.mongodb)
+const connection =mongoose.connect(process.env.mongo_DB)
 
 module.exports={connection}
---
+++ backend/utils/mail.js
@@ -0,0 +1,30 @@
+const nodemailer = require("nodemailer");
+
+require("dotenv").config
+const sendMail=async(options)=>{
+
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, 
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMPT_PASSWORD
+  },
+});
+
+  await transporter.sendMail({
+    from: process.env.SMTP_USER,
+    to:options.email,
+    subject:options.subject,
+    text:options.message,
+  })
+
+
+
+
+
+}
+
+module.exports={sendMail}
